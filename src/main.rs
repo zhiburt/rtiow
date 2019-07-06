3cb7ba5f6ebc870c85e8ac6311c465096456ff3f
Maxim Zhiburt <zhiburt@gmail.com>
2019-07-06 19:41:12 +0300

    Update to full part 5
    need more understanding

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,14 @@ mod core_grafic;
 
 use core_grafic::vec3::{RGB, Vec3, dot};
 use core_grafic::ray::Ray;
+use core_grafic::hitable::{Hitable, HitRecord};
+use core_grafic::sphere::Sphere;
+use core_grafic::hitable_list::HitableList;
 
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64 {
-    let (ray_origin_cp, center_cp) = (Vec3::copy(r.origin()), Vec3::copy(center));
-
-    let oc = &(ray_origin_cp - center_cp);
-    let a = dot(r.direction(), r.direction());
-    let b = 2.0 *  dot(oc, r.direction());
-    let c = dot(oc, oc) - radius * radius;
-    let discriminant = b*b - 4.0*a*c;
-    if discriminant < 0.0 {
-        return -1.0;
-    }
-
-    return (-b - discriminant.sqrt()) / (2.0*a);
-}
-
-fn color(r: &Ray) -> Vec3 {
-    let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5_f64, r);
-    if t > 0.0 {
-        let N = Vec3::unit_vector(&(r.point_at(t) - Vec3::new(0.0, 0.0, -1.0)));
-        return Vec3::new(N.xyz().0+1.0, N.xyz().1+1.0, N.xyz().2+1.0) * 0.5;
+fn color(r: &Ray, world: &mut Hitable) -> Vec3 {
+    let mut rec = HitRecord{t: 0.0, p: Vec3::new(0.0, 0.0, 0.0), normal: Vec3::new(0.0, 0.0, 0.0)};
+    if world.hit(r, 0.0, std::f64::MAX, &mut rec) {
+        return Vec3::new(rec.normal.xyz().0+1.0, rec.normal.xyz().1+1.0, rec.normal.xyz().2+1.0) * 0.5;
     }
 
     let unit_direction =  Vec3::unit_vector(r.direct);
@@ -36,6 +23,11 @@ fn main() {
     let (nx, ny) = (200, 100);
     let offset = 255.99;
     println!("P3\n{} {} \n255", nx, ny);
+
+    let mut hitable_list = HitableList::new(vec![
+        Box::new(Sphere::new(Vec3::new(0.0, 0.0, -1.0), 0.5)),
+        Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)),
+    ]);
 
     let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
     let horizontal = Vec3::new(4.0, 0.0, 0.0);
@@ -52,7 +44,7 @@ fn main() {
 
             let t = llc + hor * u + ver * v;
             let r = Ray::new(&origin, &t);
-            let vec = color(&r);
+            let vec = color(&r, &mut hitable_list);
 
             println!("{} {} {}", (vec[RGB::R] * offset) as isize, (vec[RGB::G] * offset) as isize, (vec[RGB::B] * offset) as isize);
         }
