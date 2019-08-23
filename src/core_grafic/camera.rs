b384da65e7cd3c1b87f65a08dd51f332fc0bcd9f
Maxim Zhiburt <maxim.zhiburt@idt.net>
2019-08-23 18:37:55 +0300

    Update camera to Chapter 10

--- src/core_grafic/camera.rs
+++ src/core_grafic/camera.rs
@@ -1,27 +1,38 @@
-use super::vec3::Vec3;
+use super::vec3::{Vec3, cross};
 use super::ray::Ray;
 
 pub struct Camera {
     origin: Vec3,
-    lofer_left_corner: Vec3,
+    lower_left_corner: Vec3,
     horizontal: Vec3,
     vertical: Vec3,
 }
 
-impl Default for Camera {
-    fn default() -> Self{
+impl Camera {
+    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3, vfov: f64, aspect: f64) -> Camera {
+        let theta = vfov * std::f64::consts::PI;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
+        
+        let w = Vec3::unit_vector(&(Vec3::copy(&lookfrom) - Vec3::copy(&lookat)));
+        let u = Vec3::unit_vector(&cross(&vup, &w));
+        let v = cross(&w, &u);
+
+        let origin = lookfrom;
+        // let lower_left_corner = Vec3::new(-half_width, -half_height, -1.0);
+        let lower_left_corner = Vec3::copy(&origin) - Vec3::copy(&u) * half_width - Vec3::copy(&v) * half_height - w;
+        let horizontal = u * 2.0 * half_width;
+        let vertical =  v * 2.0 * half_height;
         Camera{
-            lofer_left_corner: Vec3::new(-2.0, -1.0, -1.0),
-            origin: Vec3::new(0.0, 0.0, 0.0),
-            horizontal: Vec3::new(4.0, 0.0, 0.0),
-            vertical: Vec3::new(0.0, 2.0, 0.0),
+            lower_left_corner: lower_left_corner,
+            horizontal: horizontal,
+            vertical: vertical,
+            origin: origin,
         }
     }
-}
 
-impl Camera {
     pub fn get_ray(&self, u: f64, v :f64) -> Ray {
-        let llc = Vec3::copy(&self.lofer_left_corner);
+        let llc = Vec3::copy(&self.lower_left_corner);
         let h = Vec3::copy(&self.horizontal);
         let ver = Vec3::copy(&self.vertical);
         let orig = Vec3::copy(&self.origin);
